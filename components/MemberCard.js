fb4a3293844e6bb3ba43057791491a1eafa95f7a
Thomas McMahon <glassoctopus@yahoo.com>
2024-01-21 10:51:22 -0600

    form up, next is database

---
+++ components/MemberCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteMember } from '../api/memberData';
+
+function MemberCard({ memberObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE member AND HAVE THE VIEW RERENDER,
+  const deleteThisMember = () => {
+    if (window.confirm(`Delete ${memberObj.name}?`)) {
+      deleteMember(memberObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={memberObj.image} alt={memberObj.name} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Name>{memberObj.name}</Card.Name>
+        <p className="card-text bold">{memberObj.sale && <span>SALE<br /></span> } ${memberObj.price}</p>
+        {/* DYNAMIC LINK TO VIEW THE member DETAILS  */}
+        <Link href={`/member/${memberObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE member DETAILS  */}
+        <Link href={`/member/edit/${memberObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisMember} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+MemberCard.propTypes = {
+  memberObj: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    sale: PropTypes.bool,
+    price: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default MemberCard;
