d40683bb2d765459c9288292e3af419788264513
Thomas McMahon <glassoctopus@yahoo.com>
2024-01-23 18:41:45 -0600

    small edit needed

--- pages/edit/[firebaseKey].js
+++ pages/edit/[firebaseKey].js
@@ -4,19 +4,15 @@ import { getSingleMember } from '../../api/memberData';
 import MemberForm from '../../components/MemberForm';
 
 export default function EditMember() {
-  console.warn('in edit member!!!!!');
   const [member, setMember] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
 
   // TODO: make a call to the API to get the Member data
   useEffect(() => {
-    console.warn('in edit member use effect!!!!!');
     getSingleMember(firebaseKey).then((data) => {
-      console.warn('in edit member use effect then!!!!!');
       setMember(data);
     });
-    console.warn(useState);
   }, [firebaseKey]);
 
   // TODO: pass object to form
